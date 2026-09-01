cd07d639b51b80761a03add645fab7415a54c2ab
xdy1130 <61174437+xdy1130@users.noreply.github.com>
2026-09-01 17:51:00 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'slow-buy-v45';
+const CACHE = 'slow-buy-v46';
 const ASSETS = ['./', './index.html', './manifest.json', './app-icon.svg'];
 
 self.addEventListener('install', event => {
